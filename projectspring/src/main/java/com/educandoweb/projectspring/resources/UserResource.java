b9c87e9752440f56e41f381712f31f1d3efeaf94
BernardoFontesMob <bernardo.fontes@mob4pay.com.br>
2023-02-26 23:15:24 -0300

    update e post

--- projectspring/src/main/java/com/educandoweb/projectspring/resources/UserResource.java
+++ projectspring/src/main/java/com/educandoweb/projectspring/resources/UserResource.java
@@ -3,13 +3,12 @@ package com.educandoweb.projectspring.resources;
 import com.educandoweb.projectspring.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.educandoweb.projectspring.entities.User;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -27,5 +26,24 @@ public class UserResource {
     public ResponseEntity<User> findById(@PathVariable Long id) {
         User u = service.findById(id);
         return ResponseEntity.ok().body(u);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj) {
+        User u = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(u);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj){
+        obj = service.update(id, obj);
+        return ResponseEntity.ok().body(obj);
     }
 }
